da323eb0cc6c13ade0440a0d32b56cd01b866c27
Victor Baldin <victor.baldin.2005@gmail.com>
2025-05-22 10:31:52 +0300

    try to vectorize

--- src/compute.cc
+++ src/compute.cc
@@ -34,20 +34,27 @@ double computeVector(double S0, double sigma, double r, double T, double K,
   constexpr std::size_t kAlignment = kVectorSize * sizeof(double);
   assert(N % kVectorSize == 0);
 
-  std::mt19937_64 rng;
+  std::array<std::mt19937_64, kVectorSize> rngs;
+  std::array<std::lognormal_distribution<double>, kVectorSize> dists;
 
   double sigma2 = sigma * sigma;
-  std::lognormal_distribution<double> dist{(r - sigma2 / 2) * T, sigma2 * T};
   double norm = std::exp(-r * T) / N;
+
+#pragma unroll
+  for (std::size_t lane = 0; lane < kVectorSize; ++lane) {
+    rngs[lane].seed(lane);
+    dists[lane] =
+        std::lognormal_distribution<double>{(r - sigma2 / 2) * T, sigma2 * T};
+  }
 
   alignas(kAlignment) double CTs[kVectorSize]{};
 
   for (std::size_t i = 0; i < N; i += kVectorSize) {
     alignas(kAlignment) double Ys[kVectorSize];
 
-#pragma unroll
+#pragma omp simd
     for (std::size_t lane = 0; lane < kVectorSize; ++lane) {
-      Ys[lane] = dist(rng);
+      Ys[lane] = dists[lane](rngs[lane]);
     }
 
 #pragma omp simd
